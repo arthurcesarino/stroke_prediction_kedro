b73ac7e6ccb0d42bcc88b189ba93660b1f213028
arthurcesarino <contatocesarino@gmail.com>
2021-03-23 15:42:12 -0300

    Data processing and Data science pipelines

--- src/new_kedro_project/pipeline_registry.py
+++ src/new_kedro_project/pipeline_registry.py
@@ -30,7 +30,8 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline
-
+from new_kedro_project.pipelines import data_preprocessing as dp
+from new_kedro_project.pipelines import data_science as ds
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -38,4 +39,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": Pipeline([])}
+
+    data_preprocessing_pipeline = dp.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+
+    return {
+        "__default__": data_preprocessing_pipeline + data_science_pipeline,
+        "dp" : data_preprocessing_pipeline,
+        "ds" : data_science_pipeline,
+        }
